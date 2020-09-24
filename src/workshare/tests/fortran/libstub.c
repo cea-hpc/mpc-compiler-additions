9f13d8b30f7c3dff8b9f1a550e3471115f85fb0a
Thomas DIONISI <thomas.dionisi@uvsq.fr>
2020-09-24 19:11:02 +0200

    Test cleaning

--- src/workshare/tests/fortran/libstub.c
+++ src/workshare/tests/fortran/libstub.c
@@ -1,20 +1,11 @@
-#ifndef WORKSHARE_TEST_H_
-#define WORKSHARE_TEST_H_
-
 #include <stdio.h>
 #include <stdlib.h>
-#define WS_ASSERT(a, b) do { if(a != b) { fprintf(stderr, "Error: returned %d, expected %d (%s:%d)\n", a, b, __FILE__, __LINE__); abort();} } while(0)
-#define WS_ASSERT_NOT_NULL(a) do { if(a == NULL) { fprintf(stderr, "Error: "#a" is NULL(%s:%d)\n", __FILE__, __LINE__); abort();} } while(0)
-#define WS_ASSERT_TIMER(a,b) do { if(a > b) { fprintf(stderr, "Error: no stealing, loop took %lf and should take less than %2lf (%s:%d) \n",a,b,__FILE__, __LINE__); abort();} } while(0)
-
-#define WS_UNUSED(a) (void)(a)
-#define WS_SUCCESS() do {printf("SUCCESS !\n"); }while(0)
 
 extern struct
 {
    int x;
 } x_;
-int x;
+
 void MPC_Workshare_start(void (*func) (void*,long long,long long) , void* shareds,long long lb, long long ub, long long incr, int chunk_size, int steal_chunk_size, int scheduling_types, int nowait)
 {
   x_.x++;
@@ -38,4 +29,3 @@ void MPC_Workshare_critical_end()
 {
 
 }
-#endif
--- src/workshare/tests/fortran/simple.f90
+++ src/workshare/tests/fortran/simple.f90
@@ -1,14 +1,3 @@
-!MODULE arg
-!use, intrinsic :: ISO_C_BINDING
-!implicit none
-!!:: x
-!integer (c_int) :: x
-!common x
-!END MODULE arg
-!function MPC_Workshare_stop_stealing()
-!write(*,*)"BACHIBOUZOUK"
-!x=x+1
-!end
 subroutine WS_ASSERT(a,b)
   integer a,b
   if (a .ne. b) then
@@ -18,12 +7,9 @@ subroutine WS_ASSERT(a,b)
   end
 
   program main
-    !use arg
     implicit none
     integer x
     common/x/ x
-    !integer::x
-    !common x
 
     integer ( kind = 4 ) i,j,sum,sum2
     x=0 
