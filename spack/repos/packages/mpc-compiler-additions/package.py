db808cc38ffd703644bb7243839d8741b2f6dc7a
BESNARD Jean-Baptiste <jbbesnard@paratools.com>
2023-01-11 11:33:14 +0100

    GCC12: add to pipeline

--- spack/repos/packages/mpc-compiler-additions/package.py
+++ spack/repos/packages/mpc-compiler-additions/package.py
@@ -28,7 +28,7 @@ def _gcc_patch_variants():
 
 
 def _gcc_version():
-    return {"4.8.5", "6.2.0", "7.4.0", "7.3.0", "7.5.0", "8.1.0", "8.2.0", "8.3.0", "8.4.0", "9.1.0", "9.2.0", "9.3.0", "10.1.0", "10.3.0", "10.2.0", "11.1.0", "11.2.0", "11.3.0", }
+    return {"4.8.5", "6.2.0", "7.4.0", "7.3.0", "7.5.0", "8.1.0", "8.2.0", "8.3.0", "8.4.0", "9.1.0", "9.2.0", "9.3.0", "10.1.0", "10.3.0", "10.2.0", "11.1.0", "11.2.0", "11.3.0", "12.1.0", "12.2.0", }
 
 
 class MpcCompilerAdditions(AutotoolsPackage):
@@ -39,7 +39,7 @@ class MpcCompilerAdditions(AutotoolsPackage):
     homepage = "http://mpc.hpcframework.com"
     url = "https://france.paratools.com/mpc/releases/mpc-compiler-additions-0.6.2.tar.gz"
     version('0.9.0',
-            sha256='0824e1ae823919ef85287c490c0c770dbdc1f67cb27317201ffe292a44599516')
+            sha256='6674cd3d308f98b5b7bfe21109ed26ccac8ac4520c1adc7ca2c91eeab9f159f8')
 
     depends_on("hwloc@2.7.0")
     depends_on("openpa")
@@ -57,6 +57,12 @@ class MpcCompilerAdditions(AutotoolsPackage):
     variant("workshare", default=True, description="Enable workshare support")
 
     
+    conflicts("+workshare",
+              when="gcc_version=12.2.0",
+              msg="gcc_version=12.2.0 is not compatible with variant workshare")
+    conflicts("+workshare",
+              when="gcc_version=12.1.0",
+              msg="gcc_version=12.1.0 is not compatible with variant workshare")
     conflicts("+workshare",
               when="gcc_version=11.3.0",
               msg="gcc_version=11.3.0 is not compatible with variant workshare")
